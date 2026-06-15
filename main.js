aae09ccb248616a2bc10a236c36a7ead175d235c
Felipe Silva de Almeida <felipe.salmeida.008@gmail.com>
2026-06-14 22:04:32 -0300

    adiciona lógica para criação de propostas e manipulação de modal

--- main.js
+++ main.js
@@ -12,8 +12,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const db = getFirestore(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
 const adminAction = document.querySelector(".admin-action");
 const userStatusSpan = document.querySelector(".user-status span");
@@ -40,5 +40,68 @@ if (btnLogout) {
         signOut(auth).then(() => {
             window.location.href = "login.html";
         });
+    });
+}
+
+
+const modalProposta = document.getElementById("modal-proposta");
+const btnOpenModal = document.querySelector(".btn-admin-add");
+const btnCloseModal = document.getElementById("btn-close-modal");
+const formNovaProposta = document.getElementById("form-nova-proposta");
+
+if (btnOpenModal) {
+    btnOpenModal.addEventListener("click", (e) => {
+        e.preventDefault();
+        modalProposta.classList.add("active");
+    });
+}
+
+if (btnCloseModal) {
+    btnCloseModal.addEventListener("click", () => {
+        modalProposta.classList.remove("active");
+    });
+}
+
+window.addEventListener("click", (e) => {
+    if (e.target === modalProposta) {
+        modalProposta.classList.remove("active");
+    }
+});
+
+if (formNovaProposta) {
+    formNovaProposta.addEventListener("submit", async (e) => {
+        e.preventDefault();
+        
+        const usuarioLogado = auth.currentUser;
+        
+        if (usuarioLogado) {
+            try {
+                const btnSubmit = formNovaProposta.querySelector("button[type='submit']");
+                btnSubmit.textContent = "Salvando...";
+
+                await addDoc(collection(db, "propostas"), {
+                    titulo: document.getElementById("prop-titulo").value,
+                    descricao: document.getElementById("prop-desc").value,
+                    cargo: document.getElementById("prop-cargo").value,
+                    area: document.getElementById("prop-area").value,
+                    regiao: document.getElementById("prop-regiao").value,
+                    candidato: document.getElementById("prop-candidato").value,
+                    partido: document.getElementById("prop-partido").value,
+                    
+                    autorId: usuarioLogado.uid,
+                    dataCriacao: new Date().toISOString()
+                });
+
+                alert("Proposta cadastrada com sucesso!");
+                formNovaProposta.reset();
+                modalProposta.classList.remove("active");
+                btnSubmit.textContent = "Salvar no Banco de Dados";
+
+                carregarPropostas();
+            } catch (error) {
+                console.error("Erro ao salvar:", error);
+                alert("Ocorreu um erro ao salvar a proposta.");
+            }
+        }
     });
 }
